31859a30ee49265ad9592be2657652fbee7f0da2
Aliza Moin <160245045+aliza-moin18@users.noreply.github.com>
2024-02-26 23:32:22 +0500

    Add files via upload

---
+++ Q9.js
@@ -0,0 +1,3 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var num = 2024;
+console.log("my favourite number is ".concat(num));
---
+++ Q9.ts
@@ -0,0 +1,9 @@
+// Q9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+
+
+let num: number = 18
+
+console.log(`MY FAVORITE NUMBER IS ${num}`);
+
+
